ba8accfe5eaf71cb8240d63d6195b3032d5cbe64
Giorgio <1999giorgio.monne@mail.com>
2021-11-04 19:20:30 -1000

    fine esercizio con bonus

---
+++ Script.java/script.js
@@ -0,0 +1,19 @@
+
+
+const Nome = prompt('Inserisci il nome');
+console.log(Nome);
+
+const Cognome = prompt('Inserisci il cognome');
+console.log(Cognome);
+
+const ColoreP = prompt('Inserisci il tuo colore preferito');
+console.log(ColoreP);
+
+let a = '21';
+let b = '@gmail';
+let c = '.com';
+
+let email = 'Email: ' + Nome + Cognome + ColoreP + a + b + c;
+document.getElementById('email').innerHTML = email;
+console.log(email);
+
